6b1de92452682a5a085eadf0e3636e41d91f41bc
jpm <jpmckenna90@gmail.com>
2020-05-22 17:26:01 -0400

    Worked out stream syntax to allow listening to activity on a user's feed. Added conditional to check if that activity was a retweet

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 const Twit = require("twit");
+const express = require("express");
+const app = express();
+const PORT = 3000;
+
 const T = new Twit({
   consumer_key: process.env.APPLICATION_CONSUMER_KEY,
 
@@ -10,13 +14,35 @@ const T = new Twit({
 });
 
 // start stream and track tweets
-const stream = T.stream("user", { screen_name: "RyanSmith_26" });
+const stream = T.stream("statuses/filter", {
+  follow: process.env.TEST_ACCOUNT_ID,
+});
 
-// event handler - listen for tweets 
-stream.on("tweet", (tweet) => {
-  if(tweet) // if tweet is a retweet from Bill Pulte {screen_name: "pulte"}
-  {
-    // retweet and wish ryan good luck
-  } 
-  
+app.listen(PORT, function () {
+  console.log(`app listening on ${PORT}`);
+
+  // listen for any activity on the stream
+  stream.on("message", (message) => {
+    // if the message is a retweet
+    if (message.retweeted_status) {
+      // placeholder code to validate info i'll need later on
+      console.log(message.retweeted_status.user.id_str);
+      console.log(message.retweeted_status.user.name);
+    } else {
+      // just a console.log to validate conditional. to be removed
+      console.log("this was not retweeted");
+    }
+  });
+
+  // used to get user info from their tweets
+  // T.get(
+  //   "statuses/user_timeline",
+  //   { screen_name: "ryan_goodluck" },
+  //   (err, data, response) => {
+  //     if (err) {
+  //       console.log(err);
+  //     }
+  //     console.log(data);
+  //   }
+  // );
 });
